a28275c3a382e67720962060e363094845bf09f2
yuusuehara <91777581+yuusuehara@users.noreply.github.com>
2021-12-27 20:16:15 +0900

    最初のコミット

---
+++ Assets/Test.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    void Start()
+    {
+        int[] array = new int[5];
+
+        array[0] = 10;
+        array[1] = 20;
+        array[2] = 30;
+        array[3] = 40;
+        array[4] = 50;
+
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(array[i]);
+        }
+        for (int i = 4; i >= 0; i--)
+        {
+            Debug.Log(array[i]);
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
